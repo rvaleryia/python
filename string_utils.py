2f18e50bda5d71befbaff1759fb200f245552425
rvaleryia <rezviakova.lera03@gmail.com>
2025-11-03 16:28:20 +0300

    New branch for practice

---
+++ string_utils.py
@@ -0,0 +1,5 @@
+def palindrome(s: str) -> bool:
+    for i in range(len(s)//2):
+        if s[i] != s[-i -1]:
+            return False
+    return True
---
+++ test_string_utils.py
@@ -0,0 +1,11 @@
+import pytest
+from string_utils import palindrome
+import random
+
+def test_palindrome():
+    k = random.randint(1, 10)
+
+def test_sum():
+    assert 2 + 2 == 4
+
+
